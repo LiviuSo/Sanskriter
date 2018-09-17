710dd9f56f143ffeda43b008ea7b307684f13b08
lvicto <Liviu.Victor@nike.com>
2018-09-17 00:37:11 -0700

    [sanskritkeyboard] Added switch keyboard functionality (temp)

--- sanskritkeyboard/src/main/java/com/android/lvicto/sanskritkeyboard/CustomKeyboard.kt
+++ sanskritkeyboard/src/main/java/com/android/lvicto/sanskritkeyboard/CustomKeyboard.kt
@@ -10,6 +10,7 @@ import android.view.Gravity
 import android.view.KeyEvent
 import android.view.View
 import android.widget.PopupWindow
+import android.widget.RadioButton
 import android.widget.Toast
 import java.lang.Character.isLetter
 import java.lang.Character.toUpperCase
@@ -23,7 +24,10 @@ import java.lang.String.valueOf
 class CustomKeyboard : InputMethodService(), KeyboardView.OnKeyboardActionListener {
 
     private lateinit var kv: KeyboardView
-    private lateinit var keyboard: Keyboard
+    private lateinit var keyboardEn: Keyboard
+    private lateinit var keyboardRo: Keyboard
+    private lateinit var keyboardSa: Keyboard
+    private lateinit var keyboardSaIAST: Keyboard
     private lateinit var layout: View
 
     private var isCaps: Boolean = false
@@ -31,12 +35,14 @@ class CustomKeyboard : InputMethodService(), KeyboardView.OnKeyboardActionListen
     override fun onCreateInputView(): View {
         layout = layoutInflater.inflate(R.layout.keyboard, null)
         kv = layout.findViewById(R.id.keyboard) as KeyboardView
-        keyboard = Keyboard(this, R.xml.qwerty_en)
-        kv.keyboard = keyboard
+        keyboardEn = Keyboard(this, R.xml.keyboard_en)
+        keyboardRo = Keyboard(this, R.xml.keyboard_ro)
+        keyboardSa = Keyboard(this, R.xml.keyboard_sa)
+        keyboardSaIAST = Keyboard(this, R.xml.keyboard_sa_iast)
+        kv.keyboard = keyboardEn
         kv.setOnKeyboardActionListener(this)
         return layout
     }
-
 
     override fun onKey(primaryCode: Int, keyCodes: IntArray?) {
         val ic = currentInputConnection
@@ -46,7 +52,7 @@ class CustomKeyboard : InputMethodService(), KeyboardView.OnKeyboardActionListen
                 ic.deleteSurroundingText(1, 0)
             Keyboard.KEYCODE_SHIFT -> {
                 isCaps = !isCaps
-                keyboard.isShifted = isCaps
+                keyboardEn.isShifted = isCaps
                 kv.invalidateAllKeys()
             }
             Keyboard.KEYCODE_DONE -> {
@@ -97,10 +103,57 @@ class CustomKeyboard : InputMethodService(), KeyboardView.OnKeyboardActionListen
         val popup = PopupWindow(context)
         popup.contentView = layoutInflater.inflate(layout, null)
         popup.isOutsideTouchable = true
+        val radioEn = popup.contentView.findViewById<RadioButton>(R.id.rdEnglish)
+        val radioRo = popup.contentView.findViewById<RadioButton>(R.id.rdRomana)
+        val radioSa = popup.contentView.findViewById<RadioButton>(R.id.rdSanskrit)
+        val radioSaIAST = popup.contentView.findViewById<RadioButton>(R.id.rdIAST)
+
+        radioEn.setOnClickListener {
+            popup.dismiss()
+        }
+        radioRo.setOnClickListener {
+            popup.dismiss()
+        }
+        radioSa.setOnClickListener {
+            popup.dismiss()
+        }
+        radioSaIAST.setOnClickListener {
+            popup.dismiss()
+        }
+
         popup.setOnDismissListener {
-            Toast.makeText(this, "Switch keys", Toast.LENGTH_SHORT).show()
+            when {
+                radioEn.isChecked -> {
+                    setKeyboard(KeyboardType.EN)
+                }
+                radioRo.isChecked -> {
+                    setKeyboard(KeyboardType.RO)
+                }
+                radioSa.isChecked -> {
+                    setKeyboard(KeyboardType.SA)
+                }
+                radioSaIAST.isChecked -> {
+                    setKeyboard(KeyboardType.IAST)
+                }
+                else -> {
+                    // nothing
+                }
+            }
         }
         popup.showAtLocation(parent, Gravity.START and Gravity.TOP, rect[2]/4, rect[3] + 3)
+    }
+
+    private fun setKeyboard(kb: KeyboardType) {
+        when(kb) {
+            KeyboardType.EN -> kv.keyboard = keyboardEn
+            KeyboardType.RO -> kv.keyboard = keyboardRo
+            KeyboardType.SA -> kv.keyboard = keyboardSa
+            KeyboardType.IAST -> kv.keyboard = keyboardSaIAST
+            else -> {
+                // nothing
+            }
+        }
+        kv.invalidateAllKeys()
     }
 
     /**
@@ -127,6 +180,13 @@ class CustomKeyboard : InputMethodService(), KeyboardView.OnKeyboardActionListen
 
     companion object {
         private const val KEY_SWITCH_KEYBOARD = -2
+    }
+
+    enum class KeyboardType {
+        EN,
+        RO,
+        SA,
+        IAST
     }
 
 }
