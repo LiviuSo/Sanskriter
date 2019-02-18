5122a4371e5b525141976e59a930da08f2582472
LiviuSo <lsocolovici@yahoo.com>
2019-02-17 21:56:16 -0800

    [sanskritkeyboard] Added letters to qwerty keyboard phone/portrait

--- sanskritkeyboard/src/main/java/com/android/lvicto/sanskritkeyboard/CustomKeyboard2.kt
+++ sanskritkeyboard/src/main/java/com/android/lvicto/sanskritkeyboard/CustomKeyboard2.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.res.Configuration
 import android.inputmethodservice.InputMethodService
 import android.util.Log
+import android.view.KeyEvent
 import android.view.View
 import android.view.Window
 import android.view.inputmethod.EditorInfo
@@ -13,12 +14,35 @@ import android.widget.Button
 class CustomKeyboard2 : InputMethodService() {
 
     private lateinit var ic: InputConnection
-
-    companion object {
-        const val LOG_TAG = "CustomKeyboard2"
-    }
-
     private var config: Int? = null
+    private var allCaps: Boolean = false
+    private lateinit var keyQ: Button
+    private lateinit var keyW: Button
+    private lateinit var keyE: Button
+    private lateinit var keyR: Button
+    private lateinit var keyT: Button
+    private lateinit var keyY: Button
+    private lateinit var keyU: Button
+    private lateinit var keyI: Button
+    private lateinit var keyO: Button
+    private lateinit var keyP: Button
+    private lateinit var keyA: Button
+    private lateinit var keyS: Button
+    private lateinit var keyD: Button
+    private lateinit var keyF: Button
+    private lateinit var keyG: Button
+    private lateinit var keyH: Button
+    private lateinit var keyJ: Button
+    private lateinit var keyK: Button
+    private lateinit var keyL: Button
+    private lateinit var keyZ: Button
+    private lateinit var keyX: Button
+    private lateinit var keyC: Button
+    private lateinit var keyV: Button
+    private lateinit var keyB: Button
+    private lateinit var keyN: Button
+    private lateinit var keyM: Button
+
 
     override fun onConfigureWindow(win: Window?, isFullscreen: Boolean, isCandidatesOnly: Boolean) {
         Log.d(LOG_TAG, "onConfigureWindow()")
@@ -40,6 +64,7 @@ class CustomKeyboard2 : InputMethodService() {
                 Configuration.ORIENTATION_PORTRAIT -> {
                     view = layoutInflater.inflate(R.layout.keyboard_custom_layout_portrait, null)
                     initDigits(view)
+                    initLetters(view)
                 }
                 Configuration.ORIENTATION_LANDSCAPE -> {
                     view = layoutInflater.inflate(R.layout.keyboard_custom_layout_landscape, null)
@@ -108,47 +133,153 @@ class CustomKeyboard2 : InputMethodService() {
         return xlarge || large
     }
 
-    private fun initDigits(view: View) {
-        val b1 = view.findViewById<Button>(R.id.keyDigit1)
-        val b2 = view.findViewById<Button>(R.id.keyDigit2)
-        val b3 = view.findViewById<Button>(R.id.keyDigit3)
-        val b4 = view.findViewById<Button>(R.id.keyDigit4)
-        val b5 = view.findViewById<Button>(R.id.keyDigit5)
-        val b6 = view.findViewById<Button>(R.id.keyDigit6)
-        val b7 = view.findViewById<Button>(R.id.keyDigit7)
-        val b8 = view.findViewById<Button>(R.id.keyDigit8)
-        val b9 = view.findViewById<Button>(R.id.keyDigit9)
-        val b0 = view.findViewById<Button>(R.id.keyDigit0)
+    private val keyClickListener = View.OnClickListener {
+        ic.commitText((it as Button).text, 1)
+    }
 
-        b1.setOnClickListener {
-            ic.commitText((it as Button).text, 1)
+    private fun initDigits(view: View) {
+        view.findViewById<Button>(R.id.keyDigit1).apply {
+            setOnClickListener(keyClickListener)
         }
-        b2.setOnClickListener {
-            ic.commitText((it as Button).text, 1)
+        view.findViewById<Button>(R.id.keyDigit2).apply {
+            setOnClickListener(keyClickListener)
         }
-        b3.setOnClickListener {
-            ic.commitText((it as Button).text, 1)
+        view.findViewById<Button>(R.id.keyDigit3).apply {
+            setOnClickListener(keyClickListener)
         }
-        b4.setOnClickListener {
-            ic.commitText((it as Button).text, 1)
+        view.findViewById<Button>(R.id.keyDigit4).apply {
+            setOnClickListener(keyClickListener)
         }
-        b5.setOnClickListener {
-            ic.commitText((it as Button).text, 1)
+        view.findViewById<Button>(R.id.keyDigit5).apply {
+            setOnClickListener(keyClickListener)
         }
-        b6.setOnClickListener {
-            ic.commitText((it as Button).text, 1)
+        view.findViewById<Button>(R.id.keyDigit6).apply {
+            setOnClickListener(keyClickListener)
         }
-        b7.setOnClickListener {
-            ic.commitText((it as Button).text, 1)
+        view.findViewById<Button>(R.id.keyDigit7).apply {
+            setOnClickListener(keyClickListener)
         }
-        b8.setOnClickListener {
-            ic.commitText((it as Button).text, 1)
+        view.findViewById<Button>(R.id.keyDigit8).apply {
+            setOnClickListener(keyClickListener)
         }
-        b9.setOnClickListener {
-            ic.commitText((it as Button).text, 1)
+        view.findViewById<Button>(R.id.keyDigit9).apply {
+            setOnClickListener(keyClickListener)
         }
-        b0.setOnClickListener {
-            ic.commitText((it as Button).text, 1)
+        view.findViewById<Button>(R.id.keyDigit0).apply {
+            setOnClickListener(keyClickListener)
         }
+    }
+
+    private fun initLetters(view: View) {
+        keyQ = view.findViewById(R.id.keyQ)
+        keyQ.apply { setOnClickListener(keyClickListener) }
+        keyW = view.findViewById(R.id.keyW)
+        keyW.apply { setOnClickListener(keyClickListener) }
+        keyE = view.findViewById(R.id.keyE)
+        keyE.apply { setOnClickListener(keyClickListener) }
+        keyR = view.findViewById(R.id.keyR)
+        keyR.apply { setOnClickListener(keyClickListener) }
+        keyT = view.findViewById(R.id.keyT)
+        keyT.apply { setOnClickListener(keyClickListener) }
+        keyY = view.findViewById(R.id.keyY)
+        keyY.apply { setOnClickListener(keyClickListener) }
+        keyU = view.findViewById(R.id.keyU)
+        keyU.apply { setOnClickListener(keyClickListener) }
+        keyI = view.findViewById(R.id.keyI)
+        keyI.apply { setOnClickListener(keyClickListener) }
+        keyO = view.findViewById(R.id.keyO)
+        keyO.apply { setOnClickListener(keyClickListener) }
+        keyP = view.findViewById(R.id.keyP)
+        keyP.apply { setOnClickListener(keyClickListener) }
+        keyA = view.findViewById(R.id.keyA)
+        keyA.apply { setOnClickListener(keyClickListener) }
+        keyS = view.findViewById(R.id.keyS)
+        keyS.apply { setOnClickListener(keyClickListener) }
+        keyD = view.findViewById(R.id.keyD)
+        keyD.apply { setOnClickListener(keyClickListener) }
+        keyF = view.findViewById(R.id.keyF)
+        keyF.apply { setOnClickListener(keyClickListener) }
+        keyG = view.findViewById(R.id.keyG)
+        keyG.apply { setOnClickListener(keyClickListener) }
+        keyH = view.findViewById(R.id.keyH)
+        keyH.apply { setOnClickListener(keyClickListener) }
+        keyJ = view.findViewById(R.id.keyJ)
+        keyJ.apply { setOnClickListener(keyClickListener) }
+        keyK = view.findViewById(R.id.keyK)
+        keyK.apply { setOnClickListener(keyClickListener) }
+        keyL = view.findViewById(R.id.keyL)
+        keyL.apply { setOnClickListener(keyClickListener) }
+        view.findViewById<Button>(R.id.keyDel).apply {
+            setOnClickListener {
+                ic.deleteSurroundingText(1, 0)
+            }
+        }
+        keyZ = view.findViewById(R.id.keyZ)
+        keyZ.apply { setOnClickListener(keyClickListener) }
+        keyX = view.findViewById(R.id.keyX)
+        keyX.apply { setOnClickListener(keyClickListener) }
+        keyC = view.findViewById(R.id.keyC)
+        keyC.apply { setOnClickListener(keyClickListener) }
+        keyV = view.findViewById(R.id.keyV)
+        keyV.apply { setOnClickListener(keyClickListener) }
+        keyB = view.findViewById(R.id.keyB)
+        keyB.apply { setOnClickListener(keyClickListener) }
+        keyN = view.findViewById(R.id.keyN)
+        keyN.apply { setOnClickListener(keyClickListener) }
+        keyM = view.findViewById(R.id.keyM)
+        keyM.apply { setOnClickListener(keyClickListener) }
+
+        // handles only ACTION_DONE for now
+        view.findViewById<Button>(R.id.keyAction).apply {
+            setOnClickListener {
+                ic.sendKeyEvent(KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_ENTER))
+            }
+        }
+
+        view.findViewById<Button>(R.id.keyShift).apply {
+            setOnClickListener {
+                allCaps = !allCaps
+                shiftKeys(allCaps)
+            }
+        }
+    }
+
+    private fun shiftKeys(allCaps: Boolean) {
+        keyQ.text = setCase(keyQ, allCaps)
+        keyW.text = setCase(keyW, allCaps)
+        keyE.text = setCase(keyE, allCaps)
+        keyR.text = setCase(keyR, allCaps)
+        keyT.text = setCase(keyT, allCaps)
+        keyY.text = setCase(keyY, allCaps)
+        keyU.text = setCase(keyU, allCaps)
+        keyI.text = setCase(keyI, allCaps)
+        keyO.text = setCase(keyO, allCaps)
+        keyP.text = setCase(keyP, allCaps)
+        keyA.text = setCase(keyA, allCaps)
+        keyS.text = setCase(keyS, allCaps)
+        keyD.text = setCase(keyD, allCaps)
+        keyF.text = setCase(keyF, allCaps)
+        keyG.text = setCase(keyG, allCaps)
+        keyH.text = setCase(keyH, allCaps)
+        keyJ.text = setCase(keyJ, allCaps)
+        keyK.text = setCase(keyK, allCaps)
+        keyL.text = setCase(keyL, allCaps)
+        keyZ.text = setCase(keyZ, allCaps)
+        keyX.text = setCase(keyX, allCaps)
+        keyC.text = setCase(keyC, allCaps)
+        keyV.text = setCase(keyV, allCaps)
+        keyB.text = setCase(keyB, allCaps)
+        keyN.text = setCase(keyN, allCaps)
+        keyM.text = setCase(keyM, allCaps)
+    }
+
+    private fun setCase(button: Button, allCaps: Boolean): String = if(allCaps) {
+        button.text.toString().toUpperCase()
+    } else {
+        button.text.toString().toLowerCase()
+    }
+
+    companion object {
+        const val LOG_TAG = "CustomKeyboard2"
     }
 }
