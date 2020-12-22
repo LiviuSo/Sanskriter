9f5ddf5db36559eaf1347002bb38a3d4d5ab7f36
LVicto <victor.socolovici@nike.com>
2020-12-21 17:51:27 -0800

    [skeyboard] Added showPreview()

--- skeyboard/src/main/java/com/lvicto/skeyboard/view/key/TypableKeyView.kt
+++ skeyboard/src/main/java/com/lvicto/skeyboard/view/key/TypableKeyView.kt
@@ -1,11 +1,19 @@
 package com.lvicto.skeyboard.view.key
 
 import android.content.Context
+import android.graphics.Rect
 import android.util.AttributeSet
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.PopupWindow
+import android.widget.TextView
 import androidx.annotation.IntegerRes
 import com.example.skeyboard.R
 
 open class TypableKeyView(context: Context, attr: AttributeSet ) : CandidatesKeyView(context, attr) {
+
+    private lateinit var popup: PopupWindow
 
     var label: String? = ""
     @IntegerRes var labelResId: Int = -1
@@ -17,12 +25,20 @@ open class TypableKeyView(context: Context, attr: AttributeSet ) : CandidatesKey
         typedArray.recycle()
     }
 
+    /**
+     * Show key preview
+     */
     fun showPreview() {
-        // todo
+        val rect = this.locateView()
+        popup = this.createPopup(getKeyLabel())
+        popup.show(this, rect)
     }
 
+    /**
+     * Hide key preview
+     */
     fun hidePreview() {
-        // todo
+        popup.dismiss() // might not work
     }
 
     fun getKeyLabel(): String {
@@ -49,5 +65,42 @@ open class TypableKeyView(context: Context, attr: AttributeSet ) : CandidatesKey
         post {
            isEnabled = b
         }
+    }
+
+    fun View.locateView(): Rect {
+        val locInt = IntArray(2)
+        try {
+            this.getLocationInWindow(locInt)
+        } catch (npe: Throwable) {
+            // Happens when the view doesn't exist on screen anymore.
+            return Rect(-1, -1, -1, -1)
+        }
+
+        val location = Rect()
+        location.left = locInt[0]
+        location.top = locInt[1]
+        location.right = location.left + this.width
+        location.bottom = location.top + this.height
+        return location
+    }
+
+    fun View.createPopup(text: String): PopupWindow {
+        val context = this.context
+        val popup = PopupWindow(this)
+        popup.contentView = LayoutInflater.from(context).inflate(R.layout.popup_key_preview, null)
+        popup.isOutsideTouchable = true
+        popup.contentView.findViewById<TextView>(R.id.tvPreview).text = text
+        return popup
+    }
+
+    fun PopupWindow.show(parent: View, rect: Rect) {
+        val height = rect.bottom - rect.top
+        val width = rect.right - rect.left
+        val previewVertDist = parent.context.resources.getDimension(R.dimen.key_margin).toInt() // use the distance between the rows
+
+        this.showAtLocation(parent.rootView, Gravity.START or Gravity.TOP,
+                rect.left - width / 4,
+                rect.top - height - height / 2 - previewVertDist)
+        this.update(width + width / 2, height + height / 2)
     }
 }
