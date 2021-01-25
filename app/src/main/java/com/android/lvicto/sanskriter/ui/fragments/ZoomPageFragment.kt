03a45d972ebfc4fbd97ff9a166cd20d05a055154
LiviuSo <lsocolovici1979@gmail.com>
2021-01-25 04:41:42 +0200

    [app] Added functionality to search button

--- app/src/main/java/com/android/lvicto/sanskriter/ui/fragments/ZoomPageFragment.kt
+++ app/src/main/java/com/android/lvicto/sanskriter/ui/fragments/ZoomPageFragment.kt
@@ -11,7 +11,10 @@ import com.android.lvicto.db.entity.Word
 import com.android.lvicto.sanskriter.R
 import com.android.lvicto.sanskriter.util.AssetsHelper
 import com.android.lvicto.ui.AddModifyWordActivity
+import com.android.lvicto.ui.DictionaryActivity
 import com.android.lvicto.util.Constants
+import com.android.lvicto.util.Constants.Dictionary.EXTRA_WORD_EN
+import com.android.lvicto.util.Constants.Dictionary.EXTRA_WORD_IAST
 import kotlinx.android.synthetic.main.fragment_zoom_page.*
 import kotlinx.android.synthetic.main.fragment_zoom_page.view.*
 import kotlinx.android.synthetic.main.view_add_words.view.*
@@ -27,6 +30,7 @@ class ZoomPageFragment : Fragment() {
         AssetsHelper.loadAsset(viewParent.findViewById(R.id.zoomablePage), asset)
 
         viewSearchDic = inflater.inflate(R.layout.view_add_words, container, false)
+
         viewSearchDic.btnAddWord.setOnClickListener { view ->
             // move to dictionary flow
             val intent = Intent(activity, AddModifyWordActivity::class.java)
@@ -45,18 +49,35 @@ class ZoomPageFragment : Fragment() {
             llBottomToolbar.visibility = View.VISIBLE
         }
 
+        viewSearchDic.btnSearch.setOnClickListener {
+            val intent = Intent(activity, DictionaryActivity::class.java)
+            intent.putExtra(EXTRA_WORD_IAST, viewSearchDic.edWordIast.text.toString())
+            intent.putExtra(EXTRA_WORD_EN, viewSearchDic.edWordEn.text.toString())
+            startActivity(intent)
+        }
+
         viewSearchDic.btnAddWordCancel.setOnClickListener {
             bottomHolder.removeView(viewSearchDic)
             llBottomToolbar.visibility = View.VISIBLE
         }
 
         viewParent.btnAddDic.setOnClickListener {
-            if (viewParent.findViewById<ConstraintLayout>(R.id.addWordsContainer) == null) {
+            if (viewParent.findViewById<ConstraintLayout>(R.id.addSearchContainer) == null) {
                 viewParent.bottomHolder.addView(viewSearchDic)
             }
+            viewSearchDic.btnAddWord.visibility = View.VISIBLE
+            viewSearchDic.btnSearch.visibility = View.GONE
             llBottomToolbar.visibility = View.GONE
         }
 
+        viewParent.btnSearchDic.setOnClickListener {
+            if(viewParent.findViewById<ConstraintLayout>(R.id.addSearchContainer) == null) {
+                viewParent.bottomHolder.addView(viewSearchDic)
+            }
+            viewSearchDic.btnAddWord.visibility = View.GONE
+            viewSearchDic.btnSearch.visibility = View.VISIBLE
+            llBottomToolbar.visibility = View.GONE
+        }
         return viewParent
     }
 
