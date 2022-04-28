b1240a7561d7926e07aba6929b8961a9da4fc21c
root <lsocolovici@yahoo.com>
2022-04-28 17:37:36 +0300

    [dictionary] Migration step 7: Migrate delete
    (cherry picked from commit 0ed0691b8b06b6a8a22eb1c7632a03923aeae87f)

--- dictionary/src/main/java/com/android/lvicto/words/usecase/WordsDeleteUseCase.kt
+++ dictionary/src/main/java/com/android/lvicto/words/usecase/WordsDeleteUseCase.kt
@@ -1,12 +1,22 @@
 package com.android.lvicto.words.usecase
 
+import com.android.lvicto.common.WordWrapper
+import com.android.lvicto.common.concatenate
 import com.android.lvicto.db.dao.WordDao
+import com.android.lvicto.db.dao.gtypes.*
+import com.android.lvicto.db.data.GrammaticalType
 import com.android.lvicto.db.entity.Word
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
 import kotlinx.coroutines.withContext
 import java.lang.Exception
 
-class WordsDeleteUseCase(val wordDao: WordDao) {
+class WordsDeleteUseCase(val wordDao: WordDao, // todo remove
+                         private val substantiveDao: SubstantiveDao,
+                         private val pronounDao: PronounDao,
+                         private val verbsDao: VerbDao,
+                         private val numeralDao: NumeralDao,
+                         private val otherDao: OtherDao) {
 
     sealed class Result {
         object Success : Result()
@@ -16,6 +26,41 @@ class WordsDeleteUseCase(val wordDao: WordDao) {
     suspend fun deleteWords(words: List<Word>): Result = withContext(Dispatchers.IO) {
         try {
             wordDao.deleteWords(words)
+            Result.Success
+        } catch (e: Exception) {
+            Result.Failure(e.message)
+        }
+    }
+
+    suspend fun deleteWordsPlus(words: List<WordWrapper>): Result = withContext(Dispatchers.IO) {
+        val wordsByType = words.groupBy { it.gType }
+        val verbs = (wordsByType[GrammaticalType.VERB]?: listOf()).map { it.toVerb() }
+        val pronouns = (wordsByType[GrammaticalType.PRONOUN]?: listOf()).map { it.toPronoun() }
+        val numerals = concatenate(wordsByType[GrammaticalType.NUMERAL_CARDINAL] ?: listOf(),
+            wordsByType[GrammaticalType.NUMERAL_ORDINAL] ?: listOf()).map { it.toNumeral() }
+        val substantives = concatenate(wordsByType[GrammaticalType.NOUN]?: listOf(),
+            wordsByType[GrammaticalType.PROPER_NOUN]?: listOf(),
+            wordsByType[GrammaticalType.ADJECTIVE]?: listOf()).map { it.toSubstantive() }
+        val others = concatenate(wordsByType[GrammaticalType.ADVERB]?: listOf(),
+            wordsByType[GrammaticalType.INTERJECTION]?: listOf(),
+            wordsByType[GrammaticalType.PREPOSITION]?: listOf(),
+            wordsByType[GrammaticalType.SUFFIX]?: listOf(),
+            wordsByType[GrammaticalType.PREFIX]?: listOf(),
+            wordsByType[GrammaticalType.OTHER]?: listOf()).map { it.toOther() }
+
+        try {
+            val substantivesDeferred = async { substantiveDao.deleteSubstantives(substantives) }
+            val pronounDeferred = async { pronounDao.deletePronouns(pronouns) }
+            val numeralsDeferred = async { numeralDao.deleteNumerals(numerals) }
+            val verbsDeferred = async { verbsDao.deleteVerbs(verbs) }
+            val otherDeferred = async { otherDao.deleteOthers(others) }
+
+            substantivesDeferred.await()
+            pronounDeferred.await()
+            numeralsDeferred.await()
+            verbsDeferred.await()
+            otherDeferred.await()
+
             Result.Success
         } catch (e: Exception) {
             Result.Failure(e.message)
