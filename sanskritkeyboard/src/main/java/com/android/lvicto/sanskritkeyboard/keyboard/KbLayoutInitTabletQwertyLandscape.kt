494c63cf9bfc70e0ac352e1e3643dba0992114f3
Liviu Victor <lsocolovici1979@gmail.com>
2022-06-20 10:19:59 +0300

    [keyboard] Removed old sanskritkeyboard module
    [dictionary] Removed old sanskritkeyboard module 2

--- sanskritkeyboard/src/main/java/com/android/lvicto/sanskritkeyboard/keyboard/KbLayoutInitTabletQwertyLandscape.kt
+++
@@ -1,5 +0,0 @@
-package com.android.lvicto.sanskritkeyboard.keyboard
-
-import android.content.Context
-
-class KbLayoutInitTabletQwertyLandscape(context: Context) : KbLayoutInitPhoneQwertyLandscape(context)
